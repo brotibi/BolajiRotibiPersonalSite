5c3e725cf3c68a551a3adc651c0c3d1486e26b28
Bolaji <rotibi.m@husky.neu.edu>
2019-06-26 11:07:15 -0400

    Initialized the Project
    Started the React project.

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return(
+        <div className = 'Navbar'>
+            <h1>Mobolaji Rotibi</h1>
+            <h2>Projects</h2>
+            <h2>Resume</h2>
+        </div>
+    );
+};
+
+export default Navbar;
